2bedb8104e96a6b879001f6fe885a198619e8a79
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-20 10:11:26 +0600

    7th

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 
+// routes path
+const authRoutes = require('./routes/authRoutes')
 //dotenv config
 dotenv.config();
 
@@ -22,6 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 const PORT = process.env.PORT || 8080;
+//API routes
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server Running in ${process.env.DEV_MODE} mode on port no ${PORT}`.bgCyan.white);
