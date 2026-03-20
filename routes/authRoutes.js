4dfe0af1d1aab28d288cda5de7a08525d99bb286
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-20 10:06:15 +0600

    6th

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const { registerContoller, loginController, logoutCcntroller } = require('../controllers/authController')
 
 //router object
 const router = express.Router()
 
 //routes
-router.post('/register',)
+// register
+router.post('/register', registerContoller);
+
+// login
+router.post('/login', loginController);
+
+// logout
+
+router.post('/logout', logoutCcntroller);
 
 module.exports = router
