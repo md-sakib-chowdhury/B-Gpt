70d491bdb149384c1864e7a2997e49e067ae107c
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-21 10:10:24 +0600

    eid 3rd commit

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser');
 const colors = require('colors');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
-
+const errorHandler = require("./middlewares/errroMiddleware");
 // routes path
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+
 //dotenv config
 dotenv.config();
 
@@ -22,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-
+app.use(errorHandler);
 const PORT = process.env.PORT || 8080;
 //API routes
 app.use("/api/v1/auth", authRoutes);
