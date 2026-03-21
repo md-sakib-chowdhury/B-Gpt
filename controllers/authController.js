3a855f9eef1b14d769435675202b4d5fc6aa031b
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-21 10:25:15 +0600

    eid 5th

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,6 +7,21 @@ exports.sendToken = (user, statusCode, res) => {
         token,
     });
 };
-exports.registerContoller = async () => { };
+//REGISTER
+exports.registerContoller = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+        //exisitng user
+        const exisitingEmail = await userModel.findOne({ email });
+        if (exisitingEmail) {
+            return next(new errorResponse("Email is already register", 500));
+        }
+        const user = await userModel.create({ username, email, password });
+        sendToken(user, 201, res);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
 exports.loginController = async () => { };
 exports.logoutCcntroller = async () => { };
