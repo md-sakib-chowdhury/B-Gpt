5e8267722b4b6b24569894c991f6390cdc6effce
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-21 10:21:05 +0600

    eid commit 4

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,12 @@
+const userModel = require("./models/userModel");
+
+exports.sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken(res);
+    res.status(statusCode).json({
+        success: true,
+        token,
+    });
+};
 exports.registerContoller = async () => { };
 exports.loginController = async () => { };
 exports.logoutCcntroller = async () => { };
