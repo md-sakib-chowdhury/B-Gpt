52446a7a17255b63574683a0859274cbf4731642
md-sakib-chowdhury <mdsakibchowdhuryofficial00@gmail.com>
2026-03-31 08:52:55 +0600

    4th

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Box, Link, Typography } from "@mui/material";
 
-const navbar = () => {
+const Navbar = () => {
     return (
-        <div>
-            <h1>Nav menu</h1>
-        </div>
-    )
-}
+        <Box
+            width={"100%"}
+            p="1rem 6%"
+            textAlign={"center"}
+            sx={{ boxShadow: 3, mb: 2, bgcolor: "purple", color: "white" }}
+        >
+            <Typography variant="h1" color={"white"} fontWeight="bold">
+                AI GPT3 Clone
+            </Typography>
+            <Link href="/register" p={1}>
+                Sign Up
+            </Link>
+            <Link href="/login" p={1}>
+                Sign In
+            </Link>
+        </Box>
+    );
+};
 
-export default navbar
+export default Navbar;
